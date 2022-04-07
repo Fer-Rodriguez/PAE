2b51b2de7c037fa3b7abfcbae600fbf2aa1e51fb
Alejandro López <alexlohe25@gmail.com>
2022-04-07 16:20:21 -0500

    Avance 7-abr Menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,32 +1,46 @@
 import React from "react";
-import { Box, Image, Stack, HStack, VStack } from "@chakra-ui/react";
-import menuImg1 from "../../assets/menu_dash.png";
-import menuImg2 from "../../assets/menu_agenda.png";
-import menuImg3 from "../../assets/menu_cita.png";
-import menuImg4 from "../../assets/menu_user.png";
+import { Center, Image, Stack, HStack, VStack } from "@chakra-ui/react";
+import menuImg1 from "../../assets/House.png";
+import menuImg2 from "../../assets/Calendar.png";
+import menuImg3 from "../../assets/menu_user.png";
 import { isPropertyAccessChain } from "typescript";
+import { motion } from "framer-motion";
 
 function Menu() {
   return (
     <div>
       <VStack
         maxW="100px"
-        spacing="24px"
+        spacing="5px"
         bgGradient="linear(to-r, #8482FF , #A462FF)"
         borderRadius="25px"
       >
-        <Box w="80%">
-          <Image boxSize="100%" objectFit="cover" src={menuImg1} />
-        </Box>
-        <Box w="80%">
-          <Image boxSize="100%" objectFit="cover" src={menuImg2} />
-        </Box>
-        <Box w="80%">
-          <Image boxSize="100%" objectFit="cover" src={menuImg3} />
-        </Box>
-        <Box w="80%">
-          <Image boxSize="100%" objectFit="cover" src={menuImg4} />
-        </Box>
+        <motion.div
+          initial={{ width: "91%" }}
+          whileHover={{ scale: 1.1, backgroundColor: "#FFFFFF" }}
+        >
+          <Center className="container" h="80px">
+            <Image boxSize="80%" objectFit="contain" src={menuImg1} />
+          </Center>
+        </motion.div>
+
+        <motion.div
+          initial={{ width: "91%" }}
+          whileHover={{ scale: 1.1, backgroundColor: "#FFFFFF" }}
+        >
+          <Center className="container" h="80px">
+            <Image boxSize="80%" objectFit="contain" src={menuImg2} />
+          </Center>
+        </motion.div>
+
+        <motion.div
+          initial={{ width: "91%" }}
+          whileHover={{ scale: 1.1, backgroundColor: "#FFFFFF" }}
+        >
+          <Center className="container" h="80px">
+            <Image boxSize="80%" objectFit="contain" src={menuImg3} />
+          </Center>
+        </motion.div>
       </VStack>
     </div>
   );
