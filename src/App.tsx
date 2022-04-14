6c28b5a49c4aef64d0d5bf0693bc98af86fff52a
vilkas99 <victorium99@hotmail.com>
2022-04-14 00:09:39 -0500

    Fix theme conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
 //Libraries
 import { ChakraProvider, Divider, extendTheme } from "@chakra-ui/react";
-import { StepsStyleConfig as Steps } from "chakra-ui-steps";
 
 //CSS
 import "./App.css";
 import theme from "./theme";
-
-//Adding a custom component (Chakra UI Steps) into the main library of chakra, in order to use all the theme properties.
-const theme = extendTheme({
-  components: {
-    Steps,
-    Divider: {
-      defaulProps: { size: "md" },
-      sizes: {
-        lg: { borderWidth: "6px" },
-        md: { borderWidth: "4px" },
-        sm: { borderWidth: "2px" },
-      },
-    },
-  },
-});
 
 export function App() {
   return <ChakraProvider theme={theme}></ChakraProvider>;
