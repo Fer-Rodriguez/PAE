517a327b320c4d32d8f145bf5e6a50c73ef2b691
Uziel Lopez <a01733922@tec.mx>
2022-05-02 21:34:47 -0500

    Fetch data from API

--- src/api/appointments/get/index.ts
+++ src/api/appointments/get/index.ts
@@ -15,3 +15,14 @@ export const getRecentAppointment = (
 
   axios(config).then((res) => setRecentAppointment(res.data));
 };
+
+export const getAllAppointments = async (id: string, type: string) => {
+  const config = {
+    method: "get",
+    url: `http://localhost:6060/appointment/allAppointments?id=${id}&id_type=${type}`,
+  };
+
+  let response: any;
+  await axios(config).then((res) => (response = res));
+  return response;
+};
--- src/pages/Appointments/index.tsx
+++ src/pages/Appointments/index.tsx
@@ -20,9 +20,11 @@ interface IColumnDetails {
 }
 
 import { Managment } from "../Managment";
+import { getAllAppointments } from "../../api/appointments/get";
 
 export const AppointmentsPage = ({ mobile }: { mobile: boolean }) => {
   const userType = useStore((state) => state.type);
+  const userId = useStore((state) => state.id);
 
   const apiAcceptAppointment = async () => {
     await updateAppointment("4901dfc1-42c0-46bf-82f6-93e3609ae2b3", {
@@ -30,145 +32,62 @@ export const AppointmentsPage = ({ mobile }: { mobile: boolean }) => {
     });
   };
 
-  const columnsAdmin = useMemo(
-    () => [
-      {
-        Header: "Id",
-        accessor: "id",
-      },
-      {
-        Header: "Fecha",
-        accessor: "date",
-      },
-      {
-        Header: "Asesor",
-        accessor: "assessor",
-      },
-      {
-        Header: "Asesorado",
-        accessor: "student",
-      },
-      {
-        Header: "Class",
-        accessor: "class",
-      },
-      {
-        Header: "Estatus",
-        accessor: "status",
-      },
-      {
-        Header: "",
-        accessor: "accept",
-        Cell: (cell: Cell<any, any>) => (
-          <ButtonGeneric
-            text={"Aceptar"}
-            color={"purple"}
-            onClick={() =>
-              updateAppointment(cell.row.values.id, {
-                status: EStatusAppointment.ACCEPTED,
-              })
-            }
-          />
-        ),
-      },
-      {
-        Header: "",
-        accessor: "details",
-        Cell: (cell: Cell<any, any>) => (
-          <ButtonGeneric text={"Detalles"} color={"blue"} />
-        ),
-      },
-    ],
-    []
-  );
+  let dataFromAPI: IColumnDetails[] = [];
+  let columnsFromData: (
+    | {
+        Header: string;
+        accessor: string;
+        Cell?: undefined;
+      }
+    | {
+        Header: string;
+        accessor: string;
+        Cell: (cell: Cell<any, any>) => JSX.Element;
+      }
+  )[] = [];
+  getAllAppointments(userId, userType).then((res) => {
+    dataFromAPI = res.data;
+    columnsFromData = Object.keys(res.data[0]).map((key, _value) => {
+      return {
+        Header: key,
+        accessor: key,
+      };
+    });
+    console.log("COLUMNAS: " + columnsFromData);
+  });
 
-  const columns = useMemo(
-    () => [
-      {
-        Header: "Fecha",
-        accessor: "date",
-      },
-      {
-        Header: "Asesor",
-        accessor: "assessor",
-      },
-      {
-        Header: "Asesorado",
-        accessor: "student",
-      },
-      {
-        Header: "Class",
-        accessor: "class",
-      },
-      {
-        Header: "Estatus",
-        accessor: "status",
-      },
-      {
-        Header: "",
-        accessor: "details",
-        Cell: (cell: Cell<any, any>) => (
-          <ButtonGeneric text={"Detalles"} color={"blue"} />
-        ),
-      },
-    ],
-    []
-  );
+  columnsFromData.push({
+    Header: "",
+    accessor: "accept",
+    Cell: (cell: Cell<any, any>) => (
+      <ButtonGeneric
+        text={"Aceptar"}
+        color={"purple"}
+        onClick={() =>
+          updateAppointment(cell.row.values.id, {
+            status: EStatusAppointment.ACCEPTED,
+          })
+        }
+      />
+    ),
+  });
+  columnsFromData.push({
+    Header: "",
+    accessor: "details",
+    Cell: (cell: Cell<any, any>) => (
+      <ButtonGeneric text={"Detalles"} color={"blue"} />
+    ),
+  });
 
-  const tuArregloDeObjetos = [
-    {
-      id: "aasdas12321",
-      date: "16/01/2022 12:00",
-      assessor: "Julián Martinez",
-      student: "Daniela Ordan",
-      class: "Electromagnetismo",
-      status: "Pendiente",
-    },
-    {
-      id: "aasdas12321",
-      date: "15/01/2022 13:00",
-      assessor: "Alejandro Castro",
-      student: "Daniela Ordan",
-      class: "Electromagnetismo",
-      status: "Pendiente",
-    },
-    {
-      id: "aasdas12321",
-      date: "01/02/22 15:00",
-      assessor: "Fernando Jimenez",
-      student: "Daniel Maldonado",
-      class: "Linux",
-      status: "Terminada",
-    },
-    {
-      id: "aasdas12321",
-      date: "16/01/2022 10:00",
-      assessor: "Guillermo Gutierrez",
-      student: "Julián Martinez",
-      class: "Español",
-      status: "Abierta",
-    },
-    {
-      id: "aasdas12321",
-      date: "22/03/22 09:00",
-      assessor: "Benjamín Parques",
-      student: "Pedro Parques",
-      class: "Ética",
-      status: "Abierta",
-    },
-    {
-      id: "aasdas12321",
-      date: "20/04/2022 16:00",
-      assessor: "Kanye West",
-      student: "Rosalía",
-      class: "Música",
-      status: "Terminada",
-    },
-  ];
-  const data = useMemo<IColumnDetails[]>(() => tuArregloDeObjetos, []);
+  useMemo(() => {
+    columnsFromData;
+  }, []);
+
+  console.log("DATA: " + dataFromAPI);
+  const data = useMemo<IColumnDetails[]>(() => dataFromAPI, []);
   return (
     <Managment
-      columns={userType === EUserType.admin ? columnsAdmin : columns}
+      columns={columnsFromData}
       data={data}
       headColor={"pink"}
       mobile={mobile}
