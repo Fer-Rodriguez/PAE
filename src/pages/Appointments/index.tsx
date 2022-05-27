970f2ea72fd8cd01f9b1414942ad91a9324a261b
vilkas99 <victorium99@hotmail.com>
2022-05-27 11:10:11 -0500

    done

--- src/pages/Appointments/index.tsx
+++ src/pages/Appointments/index.tsx
@@ -51,6 +51,7 @@ export const AppointmentsPage = ({ mobile }: { mobile: boolean }) => {
   useEffect(() => {
     const obtainData = async () => {
       const response = await getAllAppointments(userId, userType, true);
+      console.log("Respuesta lmao:  ", response);
       setAllAppointments(response);
     };
 
