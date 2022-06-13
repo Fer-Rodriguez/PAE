5573b0d77748622ebaced61e7b538b574e6684c0
Fernando De La Torre <ref2k1@gmail.com>
2022-06-13 10:42:39 -0500

    initialTesting

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import { App } from "./App";
+import { act, render, screen } from "@testing-library/react";
+import { ButtonGeneric } from "./components/Button";
+import ReactDOM from "react-dom";
+import { Button } from "@chakra-ui/react";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let rootContainer: any;
+
+beforeEach(() => {
+  rootContainer = document.createElement("div");
+  document.body.appendChild(rootContainer);
+  ReactDOM.render(<ButtonGeneric text={"hola"} color={"red"} />, rootContainer);
+});
+
+afterEach(() => {
+  document.body.removeChild(rootContainer);
+  rootContainer.remove();
+});
+
+it("FUNCIONAAAAAAAAAAAAA", () => {
+  const button = <ButtonGeneric text={"hola"} color={"red"} />;
+  const text = "hola";
+  const color = "red";
+  expect(button).toEqual(<ButtonGeneric text={text} color={color} />);
 });
